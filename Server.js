3b105d3809604dc37686721591d7e96a6b1582c6
Farzine <farzine07@student.sust.edu>
2024-04-24 22:16:18 +0600

    added get function

--- Server.js
+++ Server.js
@@ -1,12 +1,25 @@
 // Import required modules
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
+const fs = require("fs");
+
+app.use(bodyParser.json());
+
+// Route handler
+let task = JSON.parse(fs.readFileSync("database.json"));
+
+// lists routes
+app.get("/tasks", (req, res) => {
+  // get all tasks lists
+  try {
+    res.status(200).send(task);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-    });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
